e06ca2308288db7de650555036d8d5730fcc8c3c
Brendon3421 <brendonrroger@gmail.com>
2024-12-21 10:07:28 -0300

    Subindo arquivos do dist da aplicacao

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -78,7 +78,7 @@ export default function App() {
   return (
     <div className="w-full min-h-screen bg-gray-900 flex justify-center px-4">
       <main className="my-10 w-full md:max-w-2xl">
-        <h1 className="text-4xl font-medium text-white">Clientes</h1>
+        <h1 className="text-4xl font-medium text-white">Clientes ola</h1>
 
         <form className="flex flex-col my-6" onSubmit={handleSubmit}>
           <label className="font-medium text-white">Nome:</label>
